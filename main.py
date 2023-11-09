c33fdffe7a61201ae66d91f018dfc89dd50c8b22
Sajib Ahmed <sajib.ahmed95@gmail.com>
2023-11-09 10:20:17 +1000

    Added CPI data

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import getopt
 import logging
 import openpyxl
 from datetime import datetime as dt
-
+import ausdex
 WRITE_TO_FILE=1
 
 class Security:
-    def __init__(self, ticker, units, dcaPrice, prevClose, init_AUD_exchange_rate = 1, current_AUD_exchange_rate = 1):
+    def __init__(self, ticker, units, dcaPrice, prevClose, init_buy_date, init_AUD_exchange_rate = 1, current_AUD_exchange_rate = 1):
         self.ticker = ticker
         self.units = units
         self.dcaPrice = dcaPrice
@@ -28,10 +28,11 @@ class Security:
             self.assetType = "US Market"
 
         self.initValueAUD = self.setValueAUD(self.dcaPrice, "initial")
+        self.initValueCPIAdjustedAUD = ausdex.calc_inflation(value=self.initValueAUD, original_date=init_buy_date, location="Brisbane")
         self.currValueAUD = self.setValueAUD(self.currPrice, "current")
         self.percentReturns = self.calculatePercentReturns()
 
-    def dollarCostAveragingHandler(self, units, priceBought, AUD_exchange_rate):
+    def dollarCostAveragingHandler(self, units, priceBought, AUD_exchange_rate, buy_date):
         initUnits = self.units
         initPriceBought = self.dcaPrice
         currPrice = self.currPrice
@@ -41,6 +42,7 @@ class Security:
         self.units  = initUnits + units
         self.dcaPrice = (initPriceBought * initUnits + priceBought * units) / self.units
         self.initValueAUD = initValueAUD + units * priceBought * AUD_exchange_rate
+        self.initValueCPIAdjustedAUD = self.initValueCPIAdjustedAUD + ausdex.calc_inflation(value=units * priceBought * AUD_exchange_rate, original_date=buy_date, location="Brisbane")
         self.currValueAUD = currValueAUD + units * currPrice * self.current_AUD_exchange_rate
         self.percentReturns = self.calculatePercentReturns()
 
@@ -81,12 +83,18 @@ class Security:
 
     def getInitValue(self):
         return self.initValueAUD
+    
+    def getInitCPIAdjustedValue(self):
+        return self.initValueCPIAdjustedAUD
 
     def getCurrValue(self):
         return self.currValueAUD
 
     def getPercentReturns(self):
         return self.percentReturns
+    
+    def getPercentReturnsCPIAdj(self):
+        return self.currValueAUD / self.initValueCPIAdjustedAUD * 100 - 100
 
     def setValueAUD(self, price, mode):
         if mode == "initial":
@@ -103,6 +111,15 @@ def plotPieChart(labels, value):
     ax1.axis('equal')
     plt.show()
 
+def return_date_ISO_format(date):
+    dmy = []
+    start_index = 0
+    for _ in range(3):
+        index = date.index("/", start_index) + 1
+        dmy.append(date[start_index : index])
+    print(dmy)
+
+
 def main(argv):
 
     logging.basicConfig(filename="portfolioTracker.log", encoding="utf-8", filemode="w", format="%(asctime)s - %(levelname)s: %(message)s", level=logging.DEBUG)
@@ -110,9 +127,9 @@ def main(argv):
     portValueDir = ''
     stonks = {}
     nameTickers = "AUD=X"
-    initPortValue = {"All": 0, "AUS Market": 0, "Cryptocurrency": 0, "US Market": 0}
-    currPortValue = {"All": 0, "AUS Market": 0, "Cryptocurrency": 0, "US Market": 0}
-    percPortChange = {"All": 0, "AUS Market": 0, "Cryptocurrency": 0, "US Market": 0}
+    initPortValue = {"All": 0, "All CPI Adj.": 0, "AUS Market": 0, "Cryptocurrency": 0, "US Market": 0}
+    currPortValue = {"All": 0, "All CPI Adj.": 0, "AUS Market": 0, "Cryptocurrency": 0, "US Market": 0}
+    percPortChange = {"All": 0, "All CPI Adj.": 0, "AUS Market": 0, "Cryptocurrency": 0, "US Market": 0}
     realisedProfitLoss = 0
     tickerList = []
     tickerValue = []
@@ -136,7 +153,7 @@ def main(argv):
 
     # Import initial portfolio investment and output csv as a DataFrame
     try:
-        portfolio = pd.read_excel(portfolioDir, usecols="B:H")
+        portfolio = pd.read_excel(portfolioDir, usecols="A:H")
         stocks = pd.DataFrame(portfolio)
         # Drops any empty values
         # stocks = stocks.dropna()
@@ -178,13 +195,13 @@ def main(argv):
     for ticker in stocks.itertuples():
         if ticker.Action == "BUY":
             if ticker.Ticker in stonks:
-                stonks[ticker.Ticker].dollarCostAveragingHandler(ticker.Units, ticker.Price, ticker._7)
+                stonks[ticker.Ticker].dollarCostAveragingHandler(ticker.Units, ticker.Price, ticker._8, ticker.Date)
             else:
                 dateOffset = len(dataDf['Adj Close'][ticker.Ticker].index) - 1
                 while math.isnan(dataDf['Adj Close'][ticker.Ticker].iloc[dateOffset]):
                     dateOffset -= 1
                 prevClose = dataDf['Adj Close'][ticker.Ticker].iloc[dateOffset]
-                stonks[ticker.Ticker] = Security(ticker.Ticker, ticker.Units, ticker.Price, prevClose, ticker._7, usdToAUD)
+                stonks[ticker.Ticker] = Security(ticker.Ticker, ticker.Units, ticker.Price, prevClose, ticker.Date, ticker._8, usdToAUD)
         elif ticker.Action == "SELL" or ticker.Action == "TRANSACTION":
             if ticker.Ticker in stonks:
                 if ticker.Action == "SELL":
@@ -200,28 +217,34 @@ def main(argv):
 
 
 
-    portfolioDf = {"Ticker": [], "Units Purchased": [], "Initial Price": [], "Latest Close": [], "Initial AUD Asset Value": [], "Current AUD Asset Value": [], "Percentage Returns": []}
+    portfolioDf = {"Ticker": [], "Units Purchased": [], "Initial Price": [], "Latest Close": [], "Initial AUD Asset Value": [], "Initial AUD CPI Adj. Value" : [], "Current AUD Asset Value": [], "Percentage Returns": [], "Percentage Returns CPI Adj.": []}
     for key in sorted(stonks):
         portfolioDf["Ticker"].append(stonks[key].getTicker())
         portfolioDf["Units Purchased"].append(f"{stonks[key].getUnits():.2f}")
         portfolioDf["Initial Price"].append(f"{stonks[key].getInitPrice():.2f}")
         portfolioDf["Latest Close"].append(f"{stonks[key].getCurrPrice():.2f}")
         portfolioDf["Initial AUD Asset Value"].append(f"{stonks[key].getInitValue():.2f}")
+        portfolioDf["Initial AUD CPI Adj. Value"].append(f"{stonks[key].getInitCPIAdjustedValue():.2f}")
         portfolioDf["Current AUD Asset Value"].append(f"{stonks[key].getCurrValue():.2f}")
         portfolioDf["Percentage Returns"].append(f"{stonks[key].getPercentReturns():.2f}")
+        portfolioDf["Percentage Returns CPI Adj."].append(f"{stonks[key].getPercentReturnsCPIAdj():.2f}")
         initPortValue["All"] += stonks[key].getInitValue()
+        initPortValue["All CPI Adj."] += stonks[key].getInitCPIAdjustedValue()
         initPortValue[stonks[key].getAssetType()] += stonks[key].getInitValue()
         currPortValue["All"] += stonks[key].getCurrValue()
         currPortValue[stonks[key].getAssetType()] += stonks[key].getCurrValue()
     percPortChange["All"] = currPortValue["All"] / initPortValue["All"] * 100 - 100
+    percPortChange["All CPI Adj."] = currPortValue["All"] / initPortValue["All CPI Adj."] * 100 - 100
     percPortChange["AUS Market"] = currPortValue["AUS Market"] / initPortValue["AUS Market"] * 100 - 100
     percPortChange["Cryptocurrency"] = currPortValue["Cryptocurrency"] / initPortValue["Cryptocurrency"] * 100 - 100
     percPortChange["US Market"] = currPortValue["US Market"] / initPortValue["US Market"] * 100 - 100
 
     print(pd.DataFrame(portfolioDf).to_string())
     print(f"Initial Portfolio Value is: ${initPortValue['All']:.2f}")
+    print(f"Initial Portfolio Value CPI Adj. is: ${initPortValue['All CPI Adj.']:.2f}")
     print(f"Current Portfolio Value is: ${currPortValue['All']:.2f}")
     print(f"Percentage Portfolio Performance: {percPortChange['All']:.2f}")
+    print(f"Percentage Portfolio Performance CPI Adj.: {percPortChange['All CPI Adj.']:.2f}")
     print(f"Realised Profit/Loss: ${realisedProfitLoss:.2f}AUD\n")
 
     print(f"Initial Aus Market Portfolio Value is: ${initPortValue['AUS Market']:.2f}")
